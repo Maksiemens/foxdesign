ed4586af2137224fdb35e2e7cfd596645ac14b41
Maks <exollerex@gmail.com>
2019-11-19 17:47:41 +0200

    add svg icons

--- src/blocks/modules/sign-up/sign-up.js
+++ src/blocks/modules/sign-up/sign-up.js
@@ -3,3 +3,26 @@
 // formControlUpload.addEventListener('click', () => {
 //   formInputUpload.click();
 // });
+
+
+
+/*
+https://tympanus.net/Development/ParticleEffectsButtons/
+https://github.com/codrops/ParticleEffectsButtons/
+https://github.com/codrops/ParticleEffectsButtons/
+
+
+
+http://matthew.wagerfield.com/parallax/
+
+
+http://mmkjony.github.io/enllax.js/
+
+
+jquery scroll move element plugin
+
+
+
+
+
+*/
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -3,6 +3,9 @@ import '%modules%/header/header';
 import '%modules%/intro/intro';
 import '%modules%/ideas/ideas';
 import '%modules%/quiz/quiz';
+import '%modules%/calculation/calculation';
+
+
 import '%modules%/portfolio/portfolio';
 import '%modules%/work/work';
 import '%modules%/introduce/introduce';
